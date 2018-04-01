85a430243ff52171141e278aace04c7f741cd95c
Mark Fontenot <markelliottfontenot@gmail.com>
2018-04-01 06:30:55 -0500

    updated to include mysql node package and sample code in server.js

--- projectComposeBase/node/server.js
+++ projectComposeBase/node/server.js
@@ -5,6 +5,19 @@ const Hapi = require('hapi');
 const server = new Hapi.Server();
 server.connection({ port: 3000, host: '0.0.0.0' });
 
+//Initialize the mysql variable and create the connection object with necessary values
+//Uses the https://www.npmjs.com/package/mysql package.
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+
+    //host will be the name of the service from the docker-compose file. 
+    host     : 'mysql',
+    user     : 'mark',
+    password : 'abc123',
+    database : 'cse3330'
+});
+
+
 server.route({
     method: 'GET',
     path: '/',
@@ -13,6 +26,38 @@ server.route({
         reply('Hello, world!');
     }
 });
+
+//A new route to test connectivity to MySQL
+server.route({
+    method: 'GET',
+    path: '/getData',
+    handler: function (request, reply) {
+        console.log('Server processing a /getData request');
+
+        //Creates the connection
+        connection.connect();
+
+        //Does a simple select, not from a table, but essentially just uses MySQL
+        //to add 1 + 1.
+        //function (error, results, fields){...} is a call-back function that the
+        //MySQL lib uses to send info back such as if there was an error, and/or the
+        //actual results.
+        connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+            if (error)
+                throw error;
+            //Sends back to the client the value of 1 + 1
+            reply ('The solution is ' + results[0].solution);
+
+            //for exemplar purposes, stores the returned value in a variable to be
+            //printed to log
+            var solution = results[0].solution;
+            console.log('The solution is: ', solution);
+        });
+        //close the connection to MySQL
+        connection.end();
+    }
+});
+
 
 server.route({
     method: 'GET',
