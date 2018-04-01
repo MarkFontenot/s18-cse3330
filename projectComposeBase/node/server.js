6c57f394241d0c06c843c087f9cf46e121a5e4b9
Mark Fontenot <markelliottfontenot@gmail.com>
2018-04-01 05:50:25 -0500

    Checkpoint with files before first test

---
+++ projectComposeBase/node/server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000, host: '0.0.0.0' });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        console.log('Server processing a / request');
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        console.log('Server processing /name request');
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at: ${server.info.uri}');
+});
